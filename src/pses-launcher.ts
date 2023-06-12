c7864be53a1066479e25fe3786d44472778dbf14
Lasse Meland <lasse.meland@cegal.com>
2023-06-12 12:52:39 +0200

    Remove client code and update all node modules

--- src/pses-launcher.ts
+++ src/pses-launcher.ts
@@ -2,23 +2,60 @@
  * Copyright (c) 2018 TypeFox GmbH (http://www.typefox.io). All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
-import * as rpc from "vscode-ws-jsonrpc/lib";
-import * as server from "vscode-ws-jsonrpc/lib/server";
-import * as lsp from "vscode-languageserver/lib/main";
+import {
+  WebSocketMessageReader,
+  IWebSocket,
+  WebSocketMessageWriter,
+} from "vscode-ws-jsonrpc";
+import {
+  forward,
+  createConnection,
+  createServerProcess,
+} from "vscode-ws-jsonrpc/lib/server";
+import {
+  InitializeRequest,
+  InitializeParams,
+  Message,
+} from "vscode-languageserver";
 
-export function launch(socket: rpc.IWebSocket) {
-    const reader = new rpc.WebSocketMessageReader(socket);
-    const writer = new rpc.WebSocketMessageWriter(socket);
-    // start the language server as an external process
-    const socketConnection = server.createConnection(reader, writer, () => socket.dispose());
-    const serverConnection = server.createServerProcess('powershell', 'powershell', [__dirname + '/PowerShellEditorServices/PowerShellEditorServices/Start-EditorServices.ps1', '-HostName', 'monaco', '-HostProfileId', '0', '-HostVersion', '1.0.0', '-LogPath', __dirname + '/logs/pses.log.txt', '-LogLevel', 'Diagnostic', '-BundledModulesPath', __dirname + '/PowerShellEditorServices', '-Stdio', '-SessionDetailsPath', __dirname + '/.pses_session', '-FeatureFlags', '@()']);
-    server.forward(socketConnection, serverConnection, message => {
-        if (rpc.isRequestMessage(message)) {
-            if (message.method === lsp.InitializeRequest.type.method) {
-                const initializeParams = message.params as lsp.InitializeParams;
-                initializeParams.processId = process.pid;
-            }
+export function launch(socket: IWebSocket) {
+  const reader = new WebSocketMessageReader(socket);
+  const writer = new WebSocketMessageWriter(socket);
+  // start the language server as an external process
+  const socketConnection = createConnection(reader, writer, () =>
+    socket.dispose()
+  );
+  const serverConnection = createServerProcess("powershell", "powershell", [
+    __dirname +
+      "/PowerShellEditorServices/PowerShellEditorServices/Start-EditorServices.ps1",
+    "-HostName",
+    "monaco",
+    "-HostProfileId",
+    "0",
+    "-HostVersion",
+    "1.0.0",
+    "-LogPath",
+    __dirname + "/logs/pses.log.txt",
+    "-LogLevel",
+    "Diagnostic",
+    "-BundledModulesPath",
+    __dirname + "/PowerShellEditorServices",
+    "-Stdio",
+    "-SessionDetailsPath",
+    __dirname + "/.pses_session",
+    "-FeatureFlags",
+    "@()",
+  ]);
+
+  if (serverConnection) {
+    forward(socketConnection, serverConnection, (message) => {
+      if (Message.isRequest(message)) {
+        if (message.method === InitializeRequest.type.method) {
+          const initializeParams = message.params as InitializeParams;
+          initializeParams.processId = process.pid;
         }
-        return message;
+      }
+      return message;
     });
+  }
 }
--- src/server.ts
+++ src/server.ts
@@ -10,11 +10,11 @@ import * as express from "express";
 import * as rpc from "vscode-ws-jsonrpc/lib";
 import { launch } from "./pses-launcher";
 
-process.on('uncaughtException', function (err: any) {
-    console.error('Uncaught Exception: ', err.toString());
-    if (err.stack) {
-        console.error(err.stack);
-    }
+process.on("uncaughtException", function (err: any) {
+  console.error("Uncaught Exception: ", err.toString());
+  if (err.stack) {
+    console.error(err.stack);
+  }
 });
 
 // create the express application
@@ -25,30 +25,34 @@ app.use(express.static(__dirname));
 const server = app.listen(3000);
 // create the web socket
 const wss = new ws.Server({
-    noServer: true,
-    perMessageDeflate: false
+  noServer: true,
+  perMessageDeflate: false,
 });
-server.on('upgrade', (request: http.IncomingMessage, socket: net.Socket, head: Buffer) => {
+server.on(
+  "upgrade",
+  (request: http.IncomingMessage, socket: net.Socket, head: Buffer) => {
     const pathname = request.url ? url.parse(request.url).pathname : undefined;
-    if (pathname === '/powershell') {
-        wss.handleUpgrade(request, socket, head, webSocket => {
-            const socket: rpc.IWebSocket = {
-                send: content => webSocket.send(content, error => {
-                    if (error) {
-                        throw error;
-                    }
-                }),
-                onMessage: cb => webSocket.on('message', cb),
-                onError: cb => webSocket.on('error', cb),
-                onClose: cb => webSocket.on('close', cb),
-                dispose: () => webSocket.close()
-            };
-            // launch the server when the web socket is opened
-            if (webSocket.readyState === webSocket.OPEN) {
-                launch(socket);
-            } else {
-                webSocket.on('open', () => launch(socket));
-            }
-        });
+    if (pathname === "/powershell") {
+      wss.handleUpgrade(request, socket, head, (webSocket) => {
+        const socket: rpc.IWebSocket = {
+          send: (content) =>
+            webSocket.send(content, (error) => {
+              if (error) {
+                throw error;
+              }
+            }),
+          onMessage: (cb) => webSocket.on("message", cb),
+          onError: (cb) => webSocket.on("error", cb),
+          onClose: (cb) => webSocket.on("close", cb),
+          dispose: () => webSocket.close(),
+        };
+        // launch the server when the web socket is opened
+        if (webSocket.readyState === webSocket.OPEN) {
+          launch(socket);
+        } else {
+          webSocket.on("open", () => launch(socket));
+        }
+      });
     }
-})
+  }
+);
